61e49740bbecdb89c4d58ea135457a33854ab5c7
cameronnl <33742538+cameronnl@users.noreply.github.com>
2021-02-24 21:28:54 -0800

    updating website

--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -6,15 +6,27 @@ export default function Experiences() {
         <h1 id="experiencetitle">Experiences</h1>
         <hr id="experiencedivider"></hr>
         <div className="card-wrapper">
+        <section id="projectonecard">
         <Paper elevation={3} className="card">
-            <h1>Education</h1>
+        <h1> 
+            <img src="Crest-1.png" id="smuscrest"></img> Education 
+        </h1>
+        <p> smus smus  smus smus
+        </p>
         </Paper>
+        </section>
+        <section id="projectonecard">
         <Paper elevation={3} className="card">
-            <h1>Work/Volunteer</h1>
+            <h1>
+                <i class="fas fa-hands-helping" id="helpingicon"></i>Work/Volunteer
+                </h1>
         </Paper>
+        </section>
+        <section id="projectonecard">
         <Paper elevation={3} className="card">
             <h1>Programming</h1>
         </Paper>
+        </section>
         </div>
         </div>
     )
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,15 +7,24 @@ export default function Projects() {
              <div>
             <h1 id="experiencetitle">Projects</h1>
             <hr id="experiencedivider"></hr>
+            <div className="card-wrapper">
+            <section id="projectonecard">
             <Paper elevation={3} square={false} className="card">
             <h1>hello</h1>
             </Paper>
+            </section>
+            <section id="projectonecard">
             <Paper elevation={3} className="card">
             <h1>hello</h1>
             </Paper>
+            </section>
+            <section id="projectonecard">
             <Paper elevation={3} className="card">
             <h1>hello</h1>
             </Paper>
+            </section>
+            
+        </div>
         </div>
         </div>
     )
