e26bdf6e1a52a0e1cdbf3ec80919f151611ddf2e
cameronl10 <cameron.lee@smus.ca>
2021-02-24 17:11:07 -0800

    updating website

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import Typical from 'react-typical'
+import{
+    Router,
+    Link
+} from "react-router-dom";
+
 export default function Intro() {
     return (
+        <Router>
         <div className="intropage">
             <div className="introwriting">
             <h1>Cameron Lee</h1>
@@ -12,7 +18,8 @@ export default function Intro() {
         wrapper="h1"
              />
             </div>
-            <h3 id="navigation"><a href="">About/</a><a href="">Experiences/</a><a href="">Projects/</a><a href="">Contact</a></h3>
+            <h3 id="navigation"><Link to="/Home">About</Link>/Experiences/Projects/Contact</h3>
         </div>
+        </Router>
     )
 }
